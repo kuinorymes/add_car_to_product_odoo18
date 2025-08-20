9a5e0b675738d7b7b13bf21714a52329baa191a0
Arsen Myroniuk <36411498+kuinorymes@users.noreply.github.com>
2025-08-20 18:58:06 +0300

    Update __manifest__.py

--- add_car_to_product/__manifest__.py
+++ add_car_to_product/__manifest__.py
@@ -5,6 +5,8 @@
     "description": "Allows to add car to product",
     "category": "Custom",
     "depends": ["base", "fleet", "product"],
+    "price": 10.0,
+    "currency": "EUR",
     "data": [
         "security/ir.model.access.csv",
         "security/security.xml",
