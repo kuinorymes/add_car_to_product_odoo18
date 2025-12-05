6146f3c695a2c1d4671ca08252343f280c23ed20
Arsen Myroniuk <36411498+kuinorymes@users.noreply.github.com>
2025-12-05 10:44:19 +0200

    Add license information to manifest file

--- add_car_to_product/__manifest__.py
+++ add_car_to_product/__manifest__.py
@@ -7,6 +7,7 @@
     "depends": ["base", "fleet", "product"],
     "price": 15.0,
     "currency": "EUR",
+    "license": "LGPL-3",
     "data": [
         "security/ir.model.access.csv",
         "security/security.xml",
