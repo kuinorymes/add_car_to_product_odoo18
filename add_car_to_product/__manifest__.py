7d232162a68d9eb16c6474717602ccf3f2b7da86
Arsen Myroniuk <36411498+kuinorymes@users.noreply.github.com>
2025-12-05 10:57:08 +0200

    Remove price and currency from manifest
    Removed price and currency fields from manifest.

--- add_car_to_product/__manifest__.py
+++ add_car_to_product/__manifest__.py
@@ -6,8 +6,6 @@
     "category": "Custom",
     "depends": ["base", "fleet", "product"],
     "license": "LGPL-3",
-    "price": 20.0,
-    "currency": "EUR",
     "data": [
         "security/ir.model.access.csv",
         "security/security.xml",
